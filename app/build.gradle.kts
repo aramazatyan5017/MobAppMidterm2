65b593a40c6fdecbf9d4a5b201835406d41a5f2e
Aram Azatyan <aram.azatyan@teamsolutions.am>
2023-11-15 09:47:48 +0400

    midterm2: added my solution

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.mobappmidterm2"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.mobappmidterm2"
--- app/src/main/java/com/example/mobappmidterm2/MainActivity.kt
+++ app/src/main/java/com/example/mobappmidterm2/MainActivity.kt
@@ -1,46 +1,144 @@
 package com.example.mobappmidterm2
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
 import com.example.mobappmidterm2.ui.theme.MobAppMidterm2Theme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val viewModel = ViewModelProvider(this)[UserViewModel::class.java]
         setContent {
-            MobAppMidterm2Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+            Midterm2App(viewModel = viewModel)
+        }
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun Midterm2App(viewModel: UserViewModel) {
+        viewModel.users.observe(this) {
+            setContent {
+                Column {
+                    TopAppBar(
+                        title = { Text(text = "User List") }
+                    )
+
+                    if (it.isNullOrEmpty()) {
+                        Text(text = "Loading cities...",
+                            style = MaterialTheme.typography.headlineLarge.copy(
+                                fontSize = 20.sp,
+                                fontWeight = FontWeight.Bold
+                            )
+                        )
+                    } else {
+                        UserList(users = it)
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+class UserViewModel : ViewModel() {
+    private val userService = RetrofitClient.userService
+    private val _users = MutableLiveData<List<User>>()
+    val users: LiveData<List<User>> get() = _users
+    init {
+        viewModelScope.launch {
+            try {
+                val response = userService.getUsers()
+                _users.value = response
+            } catch (e: Exception) {
+                Log.e("Retrofit", "Error While Fetching Users", e)
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    MobAppMidterm2Theme {
-        Greeting("Android")
+fun UserList(users: List<User>) {
+    LazyColumn {
+        items(users) { user ->
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp)
+                    ) {
+                        Text(
+                            text = user.name,
+                            style = MaterialTheme.typography.headlineLarge,
+                            color = Color(89, 1, 140)
+                        )
+                        Text(
+                            text = user.email,
+                            style = MaterialTheme.typography.bodyLarge,
+                            color = MaterialTheme.colorScheme.onBackground
+                        )
+                    }
+                }
+                Spacer(modifier = Modifier.height(8.dp))
+            }
+        }
     }
+}
+
+object RetrofitClient {
+    private const val BASE_URL = "https://jsonplaceholder.typicode.com/"
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+    val userService: UserService by lazy {
+        retrofit.create(UserService::class.java)
+    }
+}
+
+data class User(
+    val id: Int,
+    val name: String,
+    val email: String,
+)
+
+interface UserService {
+    @GET("users")
+    suspend fun getUsers(): List<User>
 }
